7be8a00cc9d7026454bc015df7cb9e6b5d5ede8a
Viktor Ilievski <viktor.ilievski@students.finki.ukim.mk>
2020-04-12 18:19:19 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 class Student {
 	String index;
 	String firstName;
@@ -5,14 +7,56 @@ class Student {
 	//added list
 	List<Integer> grades;
 	//TODO constructor
-
+	
+	public Student(String index, String firstName, String lastName, List<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
 	//TODO seters & getters
-
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public List<Integer> getGrades() {
+		return grades;
+	}
+	
 	public double getAverage() {
+		
+		
 		//TODO
+		for (Integer grade : grades) {
+			sum += grade
+		}
+		
+		return (double)sum/grades.size();
+		
 	}
 
 	public int ECTSCredits() {
 		//TODO
+		int krediti = 0;
+		for (Integer grade : grades) {
+			if(grade >= 6)
+				krediti += 6
+		}
+		return krediti;
 	}
 }
